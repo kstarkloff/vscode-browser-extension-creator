911bd158a9ce123c2bd4b0a3af6d4691626fba26
kstarkloff <91018072+kstarkloff@users.noreply.github.com>
2024-08-12 18:50:19 +0200

    Extension creates dummy icons.

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@
 const vscode = require('vscode');
 const path = require('path');
 const fs = require('fs');
+const { PNG } = require('pngjs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -28,11 +29,17 @@ async function createChromeExtensionFiles() {
 		{ name: 'manifest.json', content: `{
 	"manifest_version": 3,
 	"name": "My extension",
+	"description": "Description of my extension",
 	"version": "1.0",
 	"permissions": [
 	],
 	"action": {
-			"default_popup": "popup.html"
+			"default_popup": "popup.html",
+			"default_icon": {
+				"16": "images/icon16.png",
+				"48": "images/icon48.png",
+				"128": "images/icon128.png"
+			}
 	}
 }`},
 		{ name: 'popup.html', content: `<!DOCTYPE html>
@@ -59,7 +66,39 @@ async function createChromeExtensionFiles() {
 		});
 	});
 
+	// create images subdirectory
+	const imagesPath = path.join(folderPath, "images");
+
+	if (!fs.existsSync(imagesPath)) {
+		fs.mkdirSync(imagesPath);
+	}
+
+	// create png pictures
+	const png16 = new PNG({width: 16, height: 16});
+	const png48 = new PNG({width: 48, height: 48});
+	const png128 = new PNG({width: 128, height: 128});
+
+	createPngWithColor(png16, path.join(imagesPath, 'icon16.png'), 255, 0, 0, 255);
+	createPngWithColor(png48, path.join(imagesPath, 'icon48.png'), 255, 0, 0, 255);
+	createPngWithColor(png128, path.join(imagesPath, 'icon128.png'), 255, 0, 0, 255);
+
 	vscode.window.showInformationMessage('Files for Chrome extension created at ' + folderPath);
+}
+
+function createPngWithColor(png, image_path, red, green, blue, alpha) {
+	for (let y = 0; y < png.height; y++) {
+		for (let x = 0; x < png.width; x++) {
+			const idx = (png.width * y + x) << 2;
+
+			png.data[idx] = red;
+			png.data[idx + 1] = green;
+			png.data[idx + 2] = blue;
+			png.data[idx + 3] = alpha;
+		}
+	}
+
+	png.pack().pipe(fs.createWriteStream(image_path))
+		.on('finish', () => {});
 }
 
 // This method is called when your extension is deactivated
